e8521ef346b227615d0e3ab6211dbe98d15d06a9
rohitkat10 <rohitkat10@gmail.com>
2017-03-18 09:09:18 +0530

    Add theme

--- vidly/vidly/App_Start/BundleConfig.cs
+++ vidly/vidly/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace vidly
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Readablebootstarp.css",
                       "~/Content/site.css"));
         }
     }
