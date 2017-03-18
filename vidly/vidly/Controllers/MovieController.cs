61981f0d1635664608a8b3a441fb9c6e123a3197
rohitkat10 <rohitkat10@gmail.com>
2017-03-18 10:40:53 +0530

    Adding ViewModel folder & and Random Movie View Model

--- vidly/vidly/Controllers/MovieController.cs
+++ vidly/vidly/Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using vidly.Models;
+using vidly.ViewModel;
 
 namespace vidly.Controllers
 {
@@ -17,7 +18,21 @@ namespace vidly.Controllers
             {
                 Name = "RockStar"
             };
-            return View(Movie);
+
+            var Customer = new List<Customer>
+            {
+                new Customer{Name="Rohit1"},
+                new Customer{Name="Rohit2"}
+
+            };
+            var ViewModel = new RandomMovieViewModel()
+            {
+                Movie = Movie,
+                Customers = Customer
+
+            };
+           //foreach(var Customer in ViewModel. )
+            return View(ViewModel);
         }
 	}
 }
